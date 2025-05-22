5a0f9973ad486aa3b79e965fd90bd68766994ec7
Mew <mewloft@gmail.com>
2025-05-22 14:44:52 +0700

    Initial Commit

---
+++ PrimoMedium/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimoMedium
+//
+//  Created by Mew on 22/5/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PrimoMediumTests/PrimoMediumTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrimoMediumTests.swift
+//  PrimoMediumTests
+//
+//  Created by Mew on 22/5/2568 BE.
+//
+
+import Testing
+@testable import PrimoMedium
+
+struct PrimoMediumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
